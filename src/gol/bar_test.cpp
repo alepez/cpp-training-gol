28b23a2a900606aa01fc85be6967bdee1761b9b0
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-03-27 22:05:15 +0100

    pedantic warnings

--- src/gol/bar_test.cpp
+++ src/gol/bar_test.cpp
@@ -1,8 +1,9 @@
-#include <fmt/format.h>
-
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_all.hpp>
 #include <gol/bar.hpp>
+
+#include "catch2/catch_approx.hpp"
+#include "catch2/matchers/catch_matchers.hpp"
+#include "catch2/matchers/catch_matchers_string.hpp"
 
 TEST_CASE("bar test example") {
     REQUIRE(bar() == 17);
--- src/gol/foo_test.cpp
+++ src/gol/foo_test.cpp
@@ -1,8 +1,9 @@
-#include <fmt/format.h>
-
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_all.hpp>
 #include <gol/foo.hpp>
+
+#include "catch2/catch_approx.hpp"
+#include "catch2/matchers/catch_matchers.hpp"
+#include "catch2/matchers/catch_matchers_string.hpp"
 
 TEST_CASE("foo test example") {
     REQUIRE(foo() == 42);
