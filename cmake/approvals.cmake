931088327f4d363f86bf956b3cb31f2a7c3e04c5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-04-02 23:34:53 +0200

    add approvals

---
+++ cmake/approvals.cmake
@@ -0,0 +1,9 @@
+Include(FetchContent)
+
+FetchContent_Declare(
+        ApprovalTests
+        GIT_REPOSITORY https://github.com/approvals/ApprovalTests.cpp.git
+        GIT_TAG v.10.13.0
+)
+
+FetchContent_MakeAvailable(ApprovalTests)
