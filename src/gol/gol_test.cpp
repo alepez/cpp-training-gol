66b6e9e0b82cab8e4c5fd59da08b3f0c93dc53e5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-04-04 11:48:59 +0200

    Add update_grid

--- src/gol/gol_test.cpp
+++ src/gol/gol_test.cpp
@@ -10,3 +10,31 @@ using std::string;
 using std::to_string;
 using std::transform;
 using std::vector;
+
+//void update_grid(int* grid, int* grid_tmp, int w, int h);
+
+std::string to_string(int* grid, int w, int h) {
+    string result;
+    result.reserve(w * h + h);
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            result += grid[y * w + x] + '0';
+        }
+        result += "\n";
+    }
+    return result;
+}
+
+TEST_CASE("update_grid 3x3") {
+    int grid[9] = {
+        0, 0, 0,  //
+        0, 1, 0,  //
+        0, 0, 0,  //
+    };
+
+    int grid_tmp[9];
+
+    gol::update_grid(grid, grid_tmp, 3, 3);
+
+    Approvals::verify(to_string(grid, 3, 3));
+}
